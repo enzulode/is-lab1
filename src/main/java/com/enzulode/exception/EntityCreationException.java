6312eb837ff27d5af389b0bb4a9a1b8fc8a1b654
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-06 00:59:12 +0300

    chore: add serial version uid (treat spotless plugin error)

--- src/main/java/com/enzulode/exception/EntityCreationException.java
+++ src/main/java/com/enzulode/exception/EntityCreationException.java
@@ -1,6 +1,10 @@
 package com.enzulode.exception;
 
+import java.io.Serial;
+
 public class EntityCreationException extends RuntimeException {
+
+  @Serial private static final long serialVersionUID = -19831792123313L;
 
   public EntityCreationException(String message) {
     super(message);
