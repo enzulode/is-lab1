f31d21978a411ce8050e3bd97e9142a3418ebc64
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-05 20:47:09 +0300

    feat: enable k8s service discovery client

--- src/main/java/com/enzulode/OrganizationManagementApplication.java
+++ src/main/java/com/enzulode/OrganizationManagementApplication.java
@@ -2,19 +2,21 @@ package com.enzulode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Jar-level application entrypoint holder.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrganizationManagementApplication {
 
-    /**
-     * Jar-level application entrypoint.
-     *
-     * @param args command line arguments
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(OrganizationManagementApplication.class, args);
-    }
+  /**
+   * Jar-level application entrypoint.
+   *
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(OrganizationManagementApplication.class, args);
+  }
 }
