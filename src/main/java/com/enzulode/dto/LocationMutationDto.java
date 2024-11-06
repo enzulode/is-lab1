05ecf768363a398be42f60605f429dab2c1d6171
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-06 09:04:36 +0300

    feat: custom location dto for: mutating requests/responses & read reqests/responses

---
+++ src/main/java/com/enzulode/dto/LocationMutationDto.java
@@ -0,0 +1,3 @@
+package com.enzulode.dto;
+
+public record LocationMutationDto(Float x, double y, Long z) {}
---
+++ src/main/java/com/enzulode/dto/LocationReadDto.java
@@ -0,0 +1,3 @@
+package com.enzulode.dto;
+
+public record LocationReadDto(Long id, Float x, double y, Long z) {}
