0a67d1f8175907b35146326847b9d0c489558491
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-06 07:41:28 +0300

    feat: custom coordinates jpa repository methods (aka. findById & deleteById) that take coordinates record owner into account

--- src/main/java/com/enzulode/dao/repository/CoordinatesRepository.java
+++ src/main/java/com/enzulode/dao/repository/CoordinatesRepository.java
@@ -1,8 +1,14 @@
 package com.enzulode.dao.repository;
 
 import com.enzulode.dao.entity.Coordinates;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CoordinatesRepository extends JpaRepository<Coordinates, Long> {}
+public interface CoordinatesRepository extends JpaRepository<Coordinates, Long> {
+
+  Optional<Coordinates> findByIdAndCreatedBy(Long id, String username);
+
+  void deleteByIdAndCreatedBy(Long id, String username);
+}
