247d29e635a7a1f3f9325f5ee6f83e1b24686360
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-08 22:59:48 +0300

    feat: configure application-wide (presentation layer) exception handling for address service exceptions

--- src/main/java/com/enzulode/api/GlobalExceptionHandling.java
+++ src/main/java/com/enzulode/api/GlobalExceptionHandling.java
@@ -1,10 +1,7 @@
 package com.enzulode.api;
 
 import com.enzulode.dto.ErrorResponseDto;
-import com.enzulode.exception.CoordinatesCreationFailedException;
-import com.enzulode.exception.CoordinatesNotFoundException;
-import com.enzulode.exception.LocationCreationFailedException;
-import com.enzulode.exception.LocationNotFoundException;
+import com.enzulode.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,14 +12,19 @@ public class GlobalExceptionHandling {
 
   @ExceptionHandler({
     CoordinatesCreationFailedException.class,
-    LocationCreationFailedException.class
+    LocationCreationFailedException.class,
+    AddressCreationFailedException.class
   })
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleCoordinatesCreationFailure(Throwable cause) {
     return new ErrorResponseDto(cause.getMessage());
   }
 
-  @ExceptionHandler({CoordinatesNotFoundException.class, LocationNotFoundException.class})
+  @ExceptionHandler({
+    CoordinatesNotFoundException.class,
+    LocationNotFoundException.class,
+    AddressNotFoundException.class
+  })
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleCoordinatesNotFoundFailure(Throwable cause) {
     return new ErrorResponseDto(cause.getMessage());
