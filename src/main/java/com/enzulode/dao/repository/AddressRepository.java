ccdc105f303cbff1df588a46f86d089ec401c85a
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-08 22:56:28 +0300

    feat: custom address jpa repository methods (aka. findById & deleteById) that take location record owner into account

--- src/main/java/com/enzulode/dao/repository/AddressRepository.java
+++ src/main/java/com/enzulode/dao/repository/AddressRepository.java
@@ -1,8 +1,14 @@
 package com.enzulode.dao.repository;
 
 import com.enzulode.dao.entity.Address;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {}
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+  Optional<Address> findByIdAndCreatedBy(Long id, String username);
+
+  void deleteByIdAndCreatedBy(Long id, String username);
+}
