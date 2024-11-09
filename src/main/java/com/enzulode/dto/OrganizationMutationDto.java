58498813d25e02f1084a4712dd19014e3868fcab
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-09 21:42:58 +0300

    feat: custom organization dto for: mutating requests/responses & read reqests/responses

---
+++ src/main/java/com/enzulode/dto/OrganizationMutationDto.java
@@ -0,0 +1,21 @@
+package com.enzulode.dto;
+
+import com.enzulode.dao.entity.OrganizationType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+
+public record OrganizationMutationDto(
+    // formatter:off
+    String name,
+    Long coordinatesId,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    LocalDateTime creationDate,
+    Long officialAddressId,
+    int annualTurnover,
+    int employeesCount,
+    Double rating,
+    String fullName,
+    OrganizationType type,
+    Long postalAddressId
+    // formatter:on
+    ) {}
---
+++ src/main/java/com/enzulode/dto/OrganizationReadDto.java
@@ -0,0 +1,22 @@
+package com.enzulode.dto;
+
+import com.enzulode.dao.entity.OrganizationType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+
+public record OrganizationReadDto(
+    // formatter:off
+    Integer id,
+    String name,
+    CoordinatesReadDto coordinates,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    LocalDateTime creationDate,
+    AddressReadDto officialAddress,
+    int annualTurnover,
+    int employeesCount,
+    Double rating,
+    String fullName,
+    OrganizationType type,
+    AddressReadDto postalAddressId
+    // formatter:on
+    ) {}
