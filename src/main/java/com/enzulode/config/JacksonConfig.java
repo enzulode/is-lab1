5ca3a07b44d9ef23c310443d4a4dede2b4b01e52
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-09 21:41:39 +0300

    feat: configure jackson (object mapper) to be able use java time api classes within REST endpoints - added jackson java time module in the object mapper configuration

---
+++ src/main/java/com/enzulode/config/JacksonConfig.java
@@ -0,0 +1,17 @@
+package com.enzulode.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+  @Bean
+  public ObjectMapper objectMapperConfig() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    return objectMapper;
+  }
+}
