159a28876e0d554101f638c5c93f85e416ea1bec
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-09 21:44:04 +0300

    feat: custom organization jpa repository methods (aka. findById & deleteById) that take location record owner into account

--- src/main/java/com/enzulode/dao/repository/OrganizationRepository.java
+++ src/main/java/com/enzulode/dao/repository/OrganizationRepository.java
@@ -1,8 +1,14 @@
 package com.enzulode.dao.repository;
 
 import com.enzulode.dao.entity.Organization;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrganizationRepository extends JpaRepository<Organization, Integer> {}
+public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
+
+  Optional<Organization> findByIdAndCreatedBy(Integer id, String username);
+
+  void deleteByIdAndCreatedBy(Long id, String username);
+}
