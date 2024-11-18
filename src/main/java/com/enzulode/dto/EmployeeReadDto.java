448263717bc9690bc5ccf43acd4d46bd7dfb3fc7
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-19 02:08:02 +0300

    feat: employee organizations in employee read dto

--- src/main/java/com/enzulode/dto/EmployeeReadDto.java
+++ src/main/java/com/enzulode/dto/EmployeeReadDto.java
@@ -2,10 +2,12 @@ package com.enzulode.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
+import java.util.Set;
 
 public record EmployeeReadDto(
     Long id,
     String firstName,
     String lastName,
     String middleName,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") LocalDate birthDate) {}
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") LocalDate birthDate,
+    Set<OrganizationReadDto> organizations) {}
