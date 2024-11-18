41a01c2d5beb20159f2db736fd5f1f0e37639ec3
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-19 01:00:58 +0300

    feat: employee dtos mapper

---
+++ src/main/java/com/enzulode/dto/mapper/EmployeeMapper.java
@@ -0,0 +1,17 @@
+package com.enzulode.dto.mapper;
+
+import com.enzulode.dao.entity.Employee;
+import com.enzulode.dto.EmployeeCreateDto;
+import com.enzulode.dto.EmployeeReadDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface EmployeeMapper {
+
+  @Mapping(target = "id", ignore = true)
+  Employee toEntity(EmployeeCreateDto createDto);
+
+  EmployeeReadDto toReadDto(Employee employee);
+}
