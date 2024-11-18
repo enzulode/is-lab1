7f84eb8cc0c942911cbf30981d5437a9b27e7c2c
Vladislav Andreev <andreevvlad443@gmail.com>
2024-11-18 08:39:33 +0300

    feat: custom exception handling strategy for patching exceptional situations

--- src/main/java/com/enzulode/api/GlobalExceptionHandling.java
+++ src/main/java/com/enzulode/api/GlobalExceptionHandling.java
@@ -2,6 +2,7 @@ package com.enzulode.api;
 
 import com.enzulode.dto.ErrorResponseDto;
 import com.enzulode.exception.*;
+import com.enzulode.exception.patch.PatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -25,6 +26,12 @@ public class GlobalExceptionHandling {
   public ResponseEntity<ErrorResponseDto> handleUnauthorizedOperationException(
       RuntimeException cause) {
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        .body(new ErrorResponseDto(cause.getMessage()));
+  }
+
+  @ExceptionHandler({PatchException.class})
+  public ResponseEntity<ErrorResponseDto> handlePatchException(PatchException cause) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
         .body(new ErrorResponseDto(cause.getMessage()));
   }
 }
