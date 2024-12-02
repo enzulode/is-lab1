6885bdbfd25ab80e292a405483191c6c4017fe3a
Vladislav Andreev <andreevvlad443@gmail.com>
2024-12-02 22:27:11 +0300

    feat: remove unnecessary exchange, queue & binding configurations - made on the consumers with anonymous queues

--- src/main/java/com/enzulode/config/RabbitMQConfig.java
+++ src/main/java/com/enzulode/config/RabbitMQConfig.java
@@ -6,28 +6,11 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-
-  @Bean
-  public TopicExchange topicExchange(@Value("${interactive.exchange}") String exchange) {
-    return new TopicExchange(exchange);
-  }
-
-  @Bean
-  public Queue interactiveQueue(@Value("${interactive.queue}") String queue) {
-    return new Queue(queue);
-  }
-
-  @Bean
-  public Binding interactiveQueueBindingTopic(
-      Queue queue, TopicExchange exchange, @Value("${interactive.routing-key}") String routingKey) {
-    return BindingBuilder.bind(queue).to(exchange).with(routingKey);
-  }
 
   @Bean
   public MessageConverter defaultMessageConverter(ObjectMapper mapper) {
