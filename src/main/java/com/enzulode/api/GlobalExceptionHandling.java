5756632ab6d9db2c2446fb09bddf4ea66ef8f762
Vladislav Andreev <andreevvlad443@gmail.com>
2024-12-03 17:52:28 +0300

    feat: validation exception handling

--- src/main/java/com/enzulode/api/GlobalExceptionHandling.java
+++ src/main/java/com/enzulode/api/GlobalExceptionHandling.java
@@ -3,8 +3,10 @@ package com.enzulode.api;
 import com.enzulode.dto.ErrorResponseDto;
 import com.enzulode.exception.*;
 import com.enzulode.exception.patch.PatchException;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -33,5 +35,15 @@ public class GlobalExceptionHandling {
   public ResponseEntity<ErrorResponseDto> handlePatchException(PatchException cause) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
         .body(new ErrorResponseDto(cause.getMessage()));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponseDto> handleValidationExceptions(
+      MethodArgumentNotValidException cause) {
+    String errorMessage =
+        cause.getBindingResult().getFieldErrors().stream()
+            .map(error -> "%s: %s".formatted(error.getField(), error.getDefaultMessage()))
+            .collect(Collectors.joining(", "));
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDto(errorMessage));
   }
 }
